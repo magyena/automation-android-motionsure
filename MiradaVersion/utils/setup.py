d375a4792a2e590c551a9af75489b1f8470ac10e
vplusqasvr <vplus.appleid@gmail.com>
2024-06-19 16:36:01 +0700

    update

--- MiradaVersion/utils/setup.py
+++ MiradaVersion/utils/setup.py
@@ -14,7 +14,7 @@ class SetupAppium:
         options = UiAutomator2Options()
 
         # device_name = os.getenv("ANDROID_DEVICE_NAME", "cisoeqnjnnhqmr5l")
-        device_name = os.getenv("ANDROID_DEVICE_NAME", "emulator-5544")
+        device_name = os.getenv("ANDROID_DEVICE_NAME", "emulator-5556")
         options.set_capability("deviceName", device_name)
 
         # Set the app path
