0fec9e960a4fcbf85e3c3a888c6487771a4d246e
vplusqasvr <vplus.appleid@gmail.com>
2024-07-04 15:01:13 +0700

    update from master

--- MiradaVersion/test/id.Mirada.Testcase/Premium_Sports_User/TC_live_tv_premium_sports_user.py
+++ MiradaVersion/test/id.Mirada.Testcase/Premium_Sports_User/TC_live_tv_premium_sports_user.py
@@ -65,7 +65,7 @@ def test_TC_User_Watch_Live_TV_Channel_and_Disconnect_Internet(
     livetv_action.assertNoInternetConnection()
     time.sleep(3)
     profiles_action.enable_wifi_connection("emulator-5554")
-    time.sleep(5)
+    time.sleep(10)
 
 
 def test_TC_User_Can_Watching_Live_TV_Channel_Premium(
@@ -86,7 +86,7 @@ def test_TC_User_Can_Watching_Live_TV_Channel_Premium_Sports(
     livetv_action: LiveTV,
     profiles_action: Profiles,
 ):
-    for _ in range(6):
+    for _ in range(5):
         profiles_action.scroll_down()
 
     livetv_action.clickSportChannel()
